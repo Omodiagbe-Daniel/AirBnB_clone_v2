71bcbc5f80594fc509f5268fa2af64b3d48d4423
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-17 03:33:51 +0000

    more on variables with Flask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""starts and configure Flask web application"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb1():
+    """return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb():
+    """return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """return C + text"""
+    txt = text.replace('_', " ")
+    return "C " + txt
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    txt = text.replace("-", " ")
+    return "Python " + txt
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
