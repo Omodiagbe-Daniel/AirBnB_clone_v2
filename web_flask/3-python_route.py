3a6fe2bddf47325e0810a6a8a187200f93b49a56
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-17 03:39:24 +0000

    made correction

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,7 +27,7 @@ def c(text):
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    txt = text.replace("-", " ")
+    txt = text.replace("_", " ")
     return "Python " + txt
 
 
