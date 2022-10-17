e80d2652f1415ebea5dff3774b259d47e74e4204
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-17 03:20:03 +0000

    made use of variable

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hbnb1():
+    """return Hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbhb():
+    """return HBNB"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
+    """return C + text"""
     txt = text.replace('_', " ")
     return f"C {txt}"
 
