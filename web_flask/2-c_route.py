d058e86ee79d6e0ac61d3982751127f437783d49
Omodiagbe-Daniel <nosetale2017@gmail.com>
2022-10-17 03:24:06 +0000

    made use of variable

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+"""starts and configure Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def hbhb():
 def c(text):
     """return C + text"""
     txt = text.replace('_', " ")
-    return f"C {txt}"
+    return "C " + txt
 
 
 if __name__ == "__main__":
