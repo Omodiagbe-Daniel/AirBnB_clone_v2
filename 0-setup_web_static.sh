fcfc9130281a50321c1556946e9aaeb42906cefe
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-19 21:40:40 +0100

    made correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,6 +6,6 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared
 echo "nginx configuration"  | sudo tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 /etc/init.d/nginx restart
