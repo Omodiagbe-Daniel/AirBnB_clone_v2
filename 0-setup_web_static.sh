b48cdf51b72ac865f0ce626cc9848fa1bdfca91d
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-19 21:28:17 +0100

    script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static2z
+sudo apt-get -y  update
+sudo apt-get -y install nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+echo "nginx configuration" > mkdir -p /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown ubuntu /data/
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+/etc/init.d/nginx restart
