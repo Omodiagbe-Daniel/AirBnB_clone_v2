e963a250888355bc1a3fe6babc51fc8ce47e442f
root <root@OMODIAGBE-DANIEL-ONOSETALE.localdomain>
2022-12-19 21:57:32 +0100

    Fabric script that generates a .tgz archive

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fabric script that generates a .tgz archive"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Fabric script that generates a .tgz archive"""
+    local("mkdir -p version")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
